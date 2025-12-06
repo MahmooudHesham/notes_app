cc60d4ffbb229ceedec1f35209b3ba46fb54ba25
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-06 17:41:50 +0200

    create edit note view

--- lib/widgets/note_item.dart
+++ lib/widgets/note_item.dart
@@ -1,49 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edite_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-      decoration: BoxDecoration(
-        color: Colors.purple,
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        children: [
-          ListTile(
-            title: const Text(
-              "Flutter Tips",
-              style: TextStyle(color: Colors.white, fontSize: 26),
-            ),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return EditeNoteView();
+            },
+          ),
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+        decoration: BoxDecoration(
+          color: Colors.purple,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            ListTile(
+              title: const Text(
+                "Flutter Tips",
+                style: TextStyle(color: Colors.white, fontSize: 26),
+              ),
 
-            subtitle: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 16),
-              child: Text(
-                "I will became the best developer",
-                style: TextStyle(
-                  color: Colors.white.withAlpha(150),
-                  fontSize: 18,
+              subtitle: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16),
+                child: Text(
+                  "I will became the best developer",
+                  style: TextStyle(
+                    color: Colors.white.withAlpha(150),
+                    fontSize: 18,
+                  ),
                 ),
               ),
+              trailing: IconButton(
+                onPressed: () {},
+                icon: Icon(Icons.delete, color: Colors.white, size: 30),
+              ),
             ),
-            trailing: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.delete, color: Colors.white, size: 30),
-            ),
-          ),
 
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Text(
-              'APR 4,2025',
-              style: TextStyle(color: Colors.white.withAlpha(150)),
+            Padding(
+              padding: const EdgeInsets.only(right: 16),
+              child: Text(
+                'APR 4,2025',
+                style: TextStyle(color: Colors.white.withAlpha(150)),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
