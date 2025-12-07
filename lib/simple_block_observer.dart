815a339486d7e0b55bddacd83579b4d778b5ad1b
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-07 21:37:12 +0200

    add simple bloc observer

---
+++ lib/simple_block_observer.dart
@@ -0,0 +1,47 @@
+import 'package:bloc/bloc.dart';
+import 'package:flutter/foundation.dart';
+
+class SimpleBlockObserver implements BlocObserver {
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    debugPrint('chnage = $change');
+  }
+
+  @override
+  void onClose(BlocBase<dynamic> bloc) {
+    // TODO: implement onClose
+  }
+
+  @override
+  void onCreate(BlocBase<dynamic> bloc) {
+    // TODO: implement onCreate
+  }
+
+  @override
+  void onDone(
+    Bloc<dynamic, dynamic> bloc,
+    Object? event, [
+    Object? error,
+    StackTrace? stackTrace,
+  ]) {
+    // TODO: implement onDone
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    // TODO: implement onError
+  }
+
+  @override
+  void onEvent(Bloc<dynamic, dynamic> bloc, Object? event) {
+    // TODO: implement onEvent
+  }
+
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    // TODO: implement onTransition
+  }
+}
