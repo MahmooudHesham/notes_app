e11ee6ff570c85b7e9c63220aeb4ff0a37a251e4
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-08 19:05:09 +0200

    Create colors listview p1

--- lib/widgets/add_note_form.dart
+++ lib/widgets/add_note_form.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:notes_app/cubits/add_note_cubit/add_note_cubit.dart';
 import 'package:notes_app/models/note_model.dart';
+import 'package:notes_app/widgets/colors_listview.dart';
 import 'package:notes_app/widgets/custom_button.dart';
 import 'package:notes_app/widgets/cutsom_text_form_field.dart';
 import 'package:intl/intl.dart';
@@ -40,7 +41,10 @@ class _AddNoteFormState extends State<AddNoteForm> {
             hint: 'Content',
             maxLines: 5,
           ),
+          const SizedBox(height: 20),
+          const ColorsListView(),
           const SizedBox(height: 40),
+
           BlocBuilder<AddNoteCubit, AddNoteState>(
             builder: (context, state) {
               return CustomButton(
