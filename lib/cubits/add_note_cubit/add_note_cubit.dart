dad67a024d4ca65167c46de1683283ba3aa4b698
Mahmoud Hesham <7odahesham4478@gmail.com>
2025-12-08 19:42:51 +0200

    add color to new notes

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:meta/meta.dart';
 import 'package:notes_app/constants.dart';
@@ -9,7 +10,9 @@ part 'add_note_state.dart';
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitial());
 
+  Color color = Colors.purple;
   void addNote(NoteModel note) async {
+    note.color = color.toARGB32();
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNoteBox);
